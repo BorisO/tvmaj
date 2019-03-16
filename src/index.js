addfec965c0754b9c4694076b0db042ede6f283b
Boris Onufriyev <borisonufriyev@yahoo.com>
2019-03-16 15:22:42 -0400

    finished request function and tests for it.

--- src/index.js
+++ src/index.js
@@ -16,4 +16,23 @@ module.exports = class tvmaj {
     ).toString("base64");
     return authorization;
   }
+
+  // request function for api calls
+  async _request(path, version = "v1") {
+    const url = `https://api.tvmaze.com/${version}/${[path]}`;
+    const headers = {
+      Authorization: `Basic ${this.authorization}`,
+      Accept: "application/json"
+    };
+
+    // Fetch result and parse it as JSON
+    const body = await fetch(url, { headers });
+    const result = await body.json();
+
+    // Handle errors
+    if (result.status && result.status !== 200)
+      throw new Error(`${result.status}: ${result.message}`);
+
+    return result;
+  }
 };
--- test/index.test.js
+++ test/index.test.js
@@ -9,4 +9,28 @@ describe("tvmaj", () => {
       new Tvmaj();
     }).toThrow();
   });
+
+  test("_encode should return a base64 encoded string", () => {
+    const testEncode = new Tvmaj("test", "testKey");
+    const encoded = Buffer.from("test:testKey").toString("base64");
+
+    expect(testEncode.authorization).toMatch(encoded);
+  });
+
+  test("invalid request path should return 404 status and throw error", () =>
+    expect(tvmaj._request("/test/route")).rejects.toEqual(
+      new Error("404: Page not found.")
+    ));
+
+  test("unauthorized request should return 401 status and throw error", () => {
+    const testmaj = new Tvmaj("testUser", "testKey");
+    expect(testmaj._request("user/follows/shows")).rejects.toEqual(
+      new Error("401: Invalid username or API key")
+    );
+  });
+
+  test("successful requests should return object", async () => {
+    const resp = await tvmaj._request("user/follows/shows");
+    expect(resp).toBeInstanceOf(Object);
+  });
 });
